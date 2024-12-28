71c64fd3830ce09131e24cb568df5ec0d80b3cc1
pcristin <xxxozxxx@protonmail.com>
2024-12-28 13:50:23 +0300

    Added config package to handle command line flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -7,15 +7,17 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/pcristin/urlshortener/internal/app"
+	"github.com/pcristin/urlshortener/internal/config"
 )
 
 func main() {
+	config.FlagParse()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/", app.EncodeURLHandler)
 	r.Get("/{id}", app.DecodeURLHandler)
 
-	if err := http.ListenAndServe("localhost:8080", r); err != nil {
+	if err := http.ListenAndServe(config.OptionsFlag.ServerURL, r); err != nil {
 		log.Fatalf("error in ListenAndServe: %v", err)
 	}
 }
---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"flag"
+)
+
+type Options struct {
+	ServerURL string
+	ShortURL  string
+}
+
+var OptionsFlag Options
+
+func FlagParse() {
+	options := new(Options)
+
+	flag.StringVar(&options.ServerURL, "a", "localhost:8888", "address and port to run server")
+	flag.StringVar(&options.ShortURL, "b", "", "server url and short url path to redirect")
+
+	flag.Parse()
+}
