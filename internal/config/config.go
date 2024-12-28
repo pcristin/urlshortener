dbfcba5a42ecf980bda30209919e889d9852bf01
pcristin <xxxozxxx@protonmail.com>
2024-12-28 15:55:34 +0300

    Refactor configuration handling and enhance environment variable support
    - Renamed ShortURL to BaseUrl in Options struct for clarity.
    - Updated FlagParse function to log BaseUrl before and after parsing command line flags.
    - Added support for overriding ServerURL and BaseUrl using environment variables SERVER_ADDRESS and BASE_URL, respectively, improving configuration flexibility.

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,12 @@ package config
 import (
 	"flag"
 	"log"
+	"os"
 )
 
 type Options struct {
 	ServerURL string
-	ShortURL  string
+	BaseUrl   string
 }
 
 var OptionsFlag = Options{
@@ -17,12 +18,23 @@ var OptionsFlag = Options{
 func FlagParse() {
 	// Print before parsing
 	log.Printf("Before parsing - ServerURL: %s\n", OptionsFlag.ServerURL)
+	log.Printf("Before parsing - BaseURL: %s\n", OptionsFlag.BaseUrl)
 
 	flag.StringVar(&OptionsFlag.ServerURL, "a", OptionsFlag.ServerURL, "address and port to run server")
-	flag.StringVar(&OptionsFlag.ShortURL, "b", "", "server url and short url path to redirect")
+	flag.StringVar(&OptionsFlag.BaseUrl, "b", "", "server url and short url path to redirect")
 
 	flag.Parse()
 
+	// For cases when ENV variable is set, change the priority
+	for envServerUrl := os.Getenv("SERVER_ADDRESS"); envServerUrl != ""; {
+		OptionsFlag.ServerURL = envServerUrl
+	}
+
+	for envBaseUrl := os.Getenv("BASE_URL"); envBaseUrl != ""; {
+		OptionsFlag.BaseUrl = envBaseUrl
+	}
+
 	// Print after parsing
 	log.Printf("After parsing - ServerURL: %s\n", OptionsFlag.ServerURL)
+	log.Printf("After parsing - BaseURL: %s\n", OptionsFlag.BaseUrl)
 }
