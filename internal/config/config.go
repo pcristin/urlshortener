90a2cb3806098998b5ab60e11c670c977769f0cc
pcristin <xxxozxxx@protonmail.com>
2024-12-28 15:59:47 +0300

    Refactor logging and configuration in shortener application
    - Updated log statements in main.go and config.go to use carriage return for consistent line endings.
    - Renamed BaseUrl to BaseURL in Options struct for improved clarity.
    - Enhanced logging in FlagParse function to reflect changes in BaseURL and ServerURL before and after parsing command line flags.

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ import (
 
 type Options struct {
 	ServerURL string
-	BaseUrl   string
+	BaseURL   string
 }
 
 var OptionsFlag = Options{
@@ -17,24 +17,24 @@ var OptionsFlag = Options{
 
 func FlagParse() {
 	// Print before parsing
-	log.Printf("Before parsing - ServerURL: %s\n", OptionsFlag.ServerURL)
-	log.Printf("Before parsing - BaseURL: %s\n", OptionsFlag.BaseUrl)
+	log.Printf("Before parsing - ServerURL: %s\r\n", OptionsFlag.ServerURL)
+	log.Printf("Before parsing - BaseURL: %s\r\n", OptionsFlag.BaseURL)
 
 	flag.StringVar(&OptionsFlag.ServerURL, "a", OptionsFlag.ServerURL, "address and port to run server")
-	flag.StringVar(&OptionsFlag.BaseUrl, "b", "", "server url and short url path to redirect")
+	flag.StringVar(&OptionsFlag.BaseURL, "b", "", "server url and short url path to redirect")
 
 	flag.Parse()
 
 	// For cases when ENV variable is set, change the priority
-	for envServerUrl := os.Getenv("SERVER_ADDRESS"); envServerUrl != ""; {
-		OptionsFlag.ServerURL = envServerUrl
+	for envServerURL := os.Getenv("SERVER_ADDRESS"); envServerURL != ""; {
+		OptionsFlag.ServerURL = envServerURL
 	}
 
-	for envBaseUrl := os.Getenv("BASE_URL"); envBaseUrl != ""; {
-		OptionsFlag.BaseUrl = envBaseUrl
+	for envBaseURL := os.Getenv("BASE_URL"); envBaseURL != ""; {
+		OptionsFlag.BaseURL = envBaseURL
 	}
 
 	// Print after parsing
-	log.Printf("After parsing - ServerURL: %s\n", OptionsFlag.ServerURL)
-	log.Printf("After parsing - BaseURL: %s\n", OptionsFlag.BaseUrl)
+	log.Printf("After parsing - ServerURL: %s\r\n", OptionsFlag.ServerURL)
+	log.Printf("After parsing - BaseURL: %s\r\n", OptionsFlag.BaseURL)
 }
