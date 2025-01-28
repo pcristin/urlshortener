2df54d2d86c8f90c7ed8cf7aa6556995fb920074
pcristin <xxxozxxx@protonmail.com>
2025-01-28 15:53:36 +0300

    Restructered models package

--- internal/models/storage_models.go
+++ internal/models/storage_models.go
@@ -2,14 +2,6 @@ package models
 
 import "github.com/google/uuid"
 
-type Request struct {
-	URL string `json:"url"`
-}
-
-type Response struct {
-	Result string `json:"result"`
-}
-
 type URLStorageNode struct {
 	UUID        uuid.UUID `json:"uuid"`
 	ShortURL    string    `json:"short_url"`
