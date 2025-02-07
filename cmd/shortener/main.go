4805e10e92be60603b854aa02dd927d5e4e424d5
pcristin <xxxozxxx@protonmail.com>
2025-02-08 02:05:25 +0300

    Increased dbContext to 5 seconds

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -46,7 +46,7 @@ func main() {
 	}
 
 	// Initialize db context
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	// Initialize storage
