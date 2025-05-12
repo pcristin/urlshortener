ca7e7e8f402e6821d309a0b56fde74df9fcf12a3
pcristin <ozaytsev09@gmail.com>
2025-05-13 01:31:03 +0300

    fix: comments

--- internal/tls/tls.go
+++ internal/tls/tls.go
@@ -4,10 +4,11 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
+// GetTLSManager returns a new TLS manager
 func GetTLSManager() *autocert.Manager {
 	return &autocert.Manager{
-		Cache:      autocert.DirCache("cache-dir"),
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("mysite.ru", "www.mysite.ru"),
+		Cache:      autocert.DirCache("cache-dir"),                       // directory for cache
+		Prompt:     autocert.AcceptTOS,                                   // accept terms of service
+		HostPolicy: autocert.HostWhitelist("mysite.ru", "www.mysite.ru"), // allow only these hosts
 	}
 }
